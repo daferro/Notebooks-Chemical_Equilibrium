a4c2cb9410103aa51b2c5456c410f07f5c57aadd
cathedralpkg <cathedralpkg@gmail.com>
2026-04-21 11:02:19 +0200

    update files

--- subroutines/n2o4.py
+++ subroutines/n2o4.py
@@ -1,13 +1,16 @@
 # ============================================== #
-import numpy  as np
+import numpy             as np
+# ---------------------------------------------- #
+import matplotlib.pyplot as plt
+# ---------------------------------------------- #
+from   constants         import m_u,m_e,q_e,h,k_B,c_0,eps0,NA
+from   constants         import P_o,c_o,R,hbar,a_0,Eh,Hz_au
+from   constants         import NPOINTST,NPOINTSXI,REL_XI_EQ
+from   constants         import ZERO1,ZERO2,ZERO3,ZERO4
+from   constants         import FONTSIZE
 # ---------------------------------------------- #
 import general
 # ---------------------------------------------- #
-from   constants       import m_u,m_e,q_e,h,k_B,c_0,eps0,NA
-from   constants       import P_o,c_o,R,hbar,a_0,Eh,Hz_au
-from   constants       import NPOINTST,NPOINTSXI,REL_XI_EQ
-from   constants       import ZERO1,ZERO2,ZERO3,ZERO4
-from   constants       import FONTSIZE
 # ============================================== #
 
 NPOINTSYB = 51   # number of yB points (thermo & kinetics)
